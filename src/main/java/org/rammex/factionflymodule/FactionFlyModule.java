518c88da686325e30de4d2ca28701067b50e5ce2
.rammex <104463176+rammex666@users.noreply.github.com>
2025-02-16 17:26:31 +0100

    db load

--- src/main/java/org/rammex/factionflymodule/FactionFlyModule.java
+++ src/main/java/org/rammex/factionflymodule/FactionFlyModule.java
@@ -5,9 +5,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.rammex.factionflymodule.commands.FlyCommand;
 import org.rammex.factionflymodule.commands.FlyTimeCommand;
+import org.rammex.factionflymodule.data.DbManager;
 import org.rammex.factionflymodule.events.FlyEvent;
 import org.rammex.factionflymodule.placeholder.FactionFlyPlaceholder;
 import org.rammex.factionflymodule.task.FTimeTask;
+
+import java.io.File;
+
 
 public final class FactionFlyModule extends JavaPlugin {
 
@@ -17,20 +21,41 @@ public final class FactionFlyModule extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        this.getCommand("ffly").setExecutor(new FlyCommand(this));
-        this.getCommand("flytime").setExecutor(new FlyTimeCommand());
+        saveDefaultConfig();
 
-        this.getServer().getPluginManager().registerEvents(new FlyEvent(this), this);
-
-        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            new FactionFlyPlaceholder(this).register();
+        // Register commands
+        if (this.getCommand("ffly") != null) {
+            this.getCommand("ffly").setExecutor(new FlyCommand(this));
+        } else {
+            getLogger().severe("Command 'ffly' not found in plugin.yml");
         }
 
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new FTimeTask(), 20L, 20L);
+        if (this.getCommand("flytime") != null) {
+            this.getCommand("flytime").setExecutor(new FlyTimeCommand());
+        } else {
+            getLogger().severe("Command 'flytime' not found in plugin.yml");
+        }
 
+        // Register events
+        this.getServer().getPluginManager().registerEvents(new FlyEvent(this), this);
+
+        // Register PlaceholderAPI placeholder
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new FactionFlyPlaceholder(this).register();
+        } else {
+            getLogger().severe("PlaceholderAPI not found. Placeholders will not be available.");
+        }
+
+        // Initialize the database
+        DbManager database = new DbManager("factionflymodule", new File(getDataFolder(), "data.db"));
+        database.load();
+
+        // Schedule the FlyTimeTask to run every second
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new FTimeTask(), 20L, 20L);
     }
 
     @Override
     public void onDisable() {
+        // Plugin shutdown logic
     }
 }
